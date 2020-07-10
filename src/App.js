bc03713f2827278b19e712dde3d803463f198818
Rafael Escobar <rescobar@redventures.com>
2020-07-10 11:11:52 -0300

    Better design/style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const onclick = () => {
-  let r = '0000' + parseInt(Math.random() * 10000);
-  document.getElementById("result").innerText = r.substr(r.length-4);
+const calculate = () => {
+  let rand = '0000' + parseInt(Math.random() * 10000);
+  document.getElementById("result").innerText = rand.substr(rand.length-4);
 };
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <input id="input" type="text" />
-          <button id="calculate" onClick={onclick}>Calculate</button>
+      <div className="container">
+        <div className="calculator">
+          <div className="input">
+            <input id="input" type="text" />
+            <button id="calculate" onClick={calculate}>Calcular</button>
+          </div>
+          <span id="result"></span>
         </div>
-        <span id="result"></span>
+        <div className="values"></div>
       </div>
     );
   }
